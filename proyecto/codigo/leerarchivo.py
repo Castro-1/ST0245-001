0a49721e0bbaca76fb723425918f3d4c57ec7ed5
Castro-1 <82610906+Castro-1@users.noreply.github.com>
2021-08-16 23:16:37 -0500

    Add files via upload

---
+++ proyecto/codigo/leerarchivo.py
@@ -0,0 +1,5 @@
+lista=[]
+for linea in open("D:/Users/Juan Esteban Castro/Desktop/Cositas del pc/EAFIT/Java material de trabajo/Cosas Java/00000004_66324407_ver1.csv","r"):
+    lista=linea.split(",")
+    print(lista)
+linea.close()
