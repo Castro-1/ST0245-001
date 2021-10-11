3009fd0e80ac2570bc835f05b6463bfc236136a3
Castro-1 <82610906+Castro-1@users.noreply.github.com>
2021-10-10 20:56:41 -0500

    Delete leerarchivo.py

--- proyecto/codigo/leerarchivo.py
+++
@@ -1,5 +0,0 @@
-lista=[]
-for linea in open("D:/Users/Juan Esteban Castro/Desktop/Cositas del pc/EAFIT/Java material de trabajo/Cosas Java/00000004_66324407_ver1.csv","r"):
-    lista=linea.split(",")
-    print(lista)
-linea.close()
